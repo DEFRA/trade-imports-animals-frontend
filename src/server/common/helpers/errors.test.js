33e98e743e2deae89f65e02aecd0e99357da3375
andrew.harrison <andrew.harrison@Defra.onmicrosoft.com>
2026-03-02 16:33:06 +0000

    EUDPLA-1 reformatted

--- src/server/common/helpers/errors.test.js
+++ src/server/common/helpers/errors.test.js
@@ -22,9 +22,7 @@ describe('#errors', () => {
       url: '/non-existent-path'
     })
 
-    expect(result).toEqual(
-      expect.stringContaining('Page not found | Animals')
-    )
+    expect(result).toEqual(expect.stringContaining('Page not found | Animals'))
     expect(statusCode).toBe(statusCodes.notFound)
   })
 })
