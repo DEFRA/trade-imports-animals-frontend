96899ec78914a3b179d7ce0efdf32034177c247f
tarun.kumar <tarun.kumar@defra.onmicrosoft.com>
2026-04-28 16:34:17 +0100

    EUDPA-162 setup project sonarcloud config

--- src/server/port-of-entry/port-of-entry-schema.js
+++ src/server/port-of-entry/port-of-entry-schema.js
@@ -1,45 +1,52 @@
 import Joi from 'joi'
 
+const arrivalDayErrMsg = 'Enter a valid day'
+const arrivalMonthErrMsg = 'Enter a valid month'
+const arrivalYearErrMsg = 'Enter a valid year'
+const maxDays = 31
+const maxYear = 9999
+const maxMonth = 12
+
 export const portOfEntrySchema = Joi.object({
   portOfEntry: Joi.string().optional().allow('', null),
   'arrivalDate-day': Joi.number()
     .integer()
     .min(1)
-    .max(31)
+    .max(maxDays)
     .empty('')
     .optional()
     .allow(null)
     .messages({
-      'number.base': 'Enter a valid day',
-      'number.integer': 'Enter a valid day',
-      'number.min': 'Enter a valid day',
-      'number.max': 'Enter a valid day'
+      'number.base': arrivalDayErrMsg,
+      'number.integer': arrivalDayErrMsg,
+      'number.min': arrivalDayErrMsg,
+      'number.max': arrivalDayErrMsg
     }),
   'arrivalDate-month': Joi.number()
     .integer()
     .min(1)
-    .max(12)
+    .max(maxMonth)
     .empty('')
     .optional()
     .allow(null)
     .messages({
-      'number.base': 'Enter a valid month',
-      'number.integer': 'Enter a valid month',
-      'number.min': 'Enter a valid month',
-      'number.max': 'Enter a valid month'
+      'number.base': arrivalMonthErrMsg,
+      'number.integer': arrivalMonthErrMsg,
+      'number.min': arrivalMonthErrMsg,
+      'number.max': arrivalMonthErrMsg
     }),
   'arrivalDate-year': Joi.number()
     .integer()
     .min(1000)
-    .max(9999)
+    .max(maxYear)
     .empty('')
     .optional()
     .allow(null)
     .messages({
-      'number.base': 'Enter a valid year',
-      'number.integer': 'Enter a valid year',
-      'number.min': 'Enter a valid year',
-      'number.max': 'Enter a valid year'
+      'number.base': arrivalYearErrMsg,
+      'number.integer': arrivalYearErrMsg,
+      'number.min': arrivalYearErrMsg,
+      'number.max': arrivalYearErrMsg
     }),
   crumb: Joi.string().optional().allow('', null)
 })
